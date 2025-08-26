8507f56083ae508305687120fe728f50a12c89c7
AbhishekPraliya <168660055+AbhishekPraliya@users.noreply.github.com>
2025-08-26 22:11:16 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -53,7 +53,7 @@ if(process.env.NODE_ENV==="production"){
 }
 
 
-app.listen(PORT,()=>{
+app.listn(PORT || 5001,()=>{
     console.log("Hello Abhishek, Server is running on port:"+PORT);
     connectDB();
 })
